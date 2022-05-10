d7b3c2837c625aa291b66d43221319f3b4e343f9
VanThuan <nvthuan1201@gmail.com>
2022-05-10 08:02:34 +0700

    up

---
+++ Bai2/Bai2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2
+{
+    internal class Program
+    {
+        public class Animal_166
+        {
+
+            //public double Weight;
+            //public double Height;
+
+            public void Eat_166()
+            {
+                Console.WriteLine("đang ăn. . .");
+            }
+            public void MakeSound()
+            {
+                Console.WriteLine("đang phát ra tiếng kêu. . .");
+            }
+        }
+        class Cat_166 : Animal_166
+        {
+            public void Run_166()
+            {
+                Console.WriteLine("Mèo đang chạy. . .");
+            }
+            Animal_166 animal = new Animal_166();
+
+        }
+        class Bird : Animal_166
+        {
+            public void Fly_166()
+            {
+                Console.WriteLine("Chim đang bay. . .");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Cat_166 cat = new Cat_166();
+            cat.Run_166();
+            cat.Run_166();
+            Bird bird = new Bird();
+            bird.Fly_166();
+            bird.MakeSound();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DiemDanh10-05/DiemDanh10-05/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiemDanh10_05
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a166 = new int[100];
+            int i166, max166, min166, n166;
+            Console.Write("\nTim phan tu lon nhat, phan tu nho nhat trong mang trong C#:\n");
+            Console.Write("-----------------------------------------------------------\n");
+            Console.Write("Nhap so phan tu can luu giu trong mang: ");
+            n166 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap {0} phan tu vao trong mang:\n", n166);
+            for (i166 = 0; i166 < n166; i166++)
+            {
+                Console.Write("Phan tu {0}: ", i166);
+                a166[i166] = Convert.ToInt32(Console.ReadLine());
+            }
+            max166 = a166[0];
+            min166 = a166[0];
+            for (i166 = 1; i166 < n166; i166++)
+            {
+                if (a166[i166] > max166)
+                {
+                    max166 = a166[i166];
+                }
+                if (a166[i166] < min166)
+                {
+                    min166 = a166[i166];
+                }
+            }
+            Console.Write("Phan tu lon nhat trong mang la: {0}\n", max166);
+            Console.Write("Phan tu nho nhat trong mang la: {0}\n\n", min166);
+
+            Console.ReadKey();
+        }
+    }
+}
